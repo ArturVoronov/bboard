76c77f4ba0eac553a50d989e5d5a6af718446207
Artur Voronov <artur.voronov.1979@mail.ru>
2024-10-12 21:17:24 +0300

    doing with book

---
+++ samplesite/bboard/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
+
+def index(request):
+    template = loader.get_template ('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context ={'bbs':bbs}
+    return HttpResponse(template.render(context, request))
+
+
+    """s = 'Объявления\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content +'\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+"""
