fcd4c293c573fe0cab22d8cb0e2dd5b267f75634
Artur Voronov <artur.voronov.1979@mail.ru>
2024-10-16 19:03:29 +0300

    61 page

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Bb
+from .models import Bb,Rubric
 
 def index(request):
     template = loader.get_template ('bboard/index.html')
@@ -10,7 +10,12 @@ def index(request):
     context ={'bbs':bbs}
     return HttpResponse(template.render(context, request))
 
-
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs':bbs, 'rubrics': rubrics, 'current_rubric':current_rubric}
+    return render(request,'bboard/rubric_bbs.html', context)
     """s = 'Объявления\r\n\r\n\r\n'
     for bb in Bb.objects.order_by('-published'):
         s += bb.title + '\r\n' + bb.content +'\r\n\r\n'
